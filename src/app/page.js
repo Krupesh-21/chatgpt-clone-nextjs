bb2c893ba92bf0c05a8ba6af151464c5b4053aec
Krupesh Joshi <krupeshjoshi21@gmail.com>
2024-01-29 19:09:27 +0530

    lates chat changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
 import { v4 as uuid } from "uuid";
 
 import { FaArrowUpLong } from "react-icons/fa6";
@@ -10,9 +11,26 @@ import { GoPaste } from "react-icons/go";
 import { IoMdCheckmark } from "react-icons/io";
 import { IoPerson } from "react-icons/io5";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { PiDotsThreeBold } from "react-icons/pi";
 import { SiSendinblue } from "react-icons/si";
 
-import CopyToClipboard from "react-copy-to-clipboard";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import "./app.css";
 
 const HomePage = () => {
@@ -26,6 +44,7 @@ const HomePage = () => {
   );
   const [currentSelectedHistoryId, setCurrentSelectedHistoryId] =
     useState(null);
+  const [showPriceModal, setShowPriceModal] = useState(false);
 
   const promptRef = useRef(null);
   const bottomRef = useRef(null);
@@ -223,6 +242,10 @@ const HomePage = () => {
     }
   };
 
+  const togglePricingModal = () => {
+    setShowPriceModal((prev) => !prev);
+  };
+
   useEffect(() => {
     const textarea = promptRef.current;
 
@@ -267,127 +290,204 @@ const HomePage = () => {
   }, [gptResponseCopied]);
 
   return (
-    <div id="content">
-      <div className="sidebar-container">
-        <div className="new-chat-container" onClick={handleNewChatClick}>
-          <div className="new-chat">
-            <div className="icon">
-              <SiSendinblue />
-            </div>
-            <span>New Chat</span>
-          </div>
-          <div className="edit-icon">
-            <FiEdit />
-          </div>
-        </div>
-        <div className="chat-history-container">
-          {chatHistory.length > 0
-            ? chatHistory.map((item) => (
-                <div
-                  key={item.historyId}
-                  className="title-container"
-                  onClick={() => handleOldChatClick(item.historyId)}
-                >
-                  <p className="title">{item.title}</p>
-                </div>
-              ))
-            : null}
-        </div>
-      </div>
-
-      <div className="chat-container">
-        <div className="header">
-          <div className="title-version">
-            <span>ChatGPT 3.5</span>
-            <MdOutlineKeyboardArrowDown />
-          </div>
-        </div>
-        <div className="chat-response-container">
-          {history.length > 0 ? (
-            history.map((item, index) => (
-              <>
-                <div
-                  key={index + 1}
-                  className={clsx("response-container", item.role)}
-                >
-                  <div
-                    className={clsx("chat-response", {
-                      "show-edit-icon": item.role === "user",
-                    })}
-                  >
-                    <div className="user-details">
-                      <div className="icon">
-                        {item.role === "user" ? <IoPerson /> : <SiSendinblue />}
-                      </div>
-                      <span>{item.role === "user" ? "You" : "ChatGPT"}</span>
-                    </div>
-                    <div className="response">
-                      {item.role === "user" ? (
-                        <p>{item.parts}</p>
-                      ) : (
-                        item.parts
-                          .split(/(```[^`]+```)/)
-                          .map((data) => renderGptResponse(data))
-                      )}
-                    </div>
-                    <div className="tools">
-                      {item.role === "user" ? (
-                        <FiEdit2 />
-                      ) : (
-                        <CopyToClipboard
-                          text={item.parts}
-                          onCopy={(text, result) =>
-                            setGptResponseCopied(result)
-                          }
-                        >
-                          {gptResponseCopied ? <IoMdCheckmark /> : <GoPaste />}
-                        </CopyToClipboard>
-                      )}
-                    </div>
-                  </div>
-                </div>
-                {index === history.length - 1 && (
-                  <>
-                    <div className="last-section" />
-                    <div ref={bottomRef}></div>
-                  </>
-                )}
-              </>
-            ))
-          ) : (
-            <div className="default-screen">
+    <>
+      <div id="content">
+        <div className="sidebar-container">
+          <div
+            className="new-chat-container hover:bg-stone-300/15"
+            onClick={handleNewChatClick}
+          >
+            <div className="new-chat">
               <div className="icon">
                 <SiSendinblue />
               </div>
-              <h2>How can I help you today?</h2>
+              <span>New Chat</span>
             </div>
-          )}
-        </div>
-
-        <div className="input-container">
-          <div className="interact">
-            <textarea
-              type="text"
-              name="prompt"
-              rows={1}
-              ref={promptRef}
-              onInput={(e) => {
-                promptRef.current.style.height = "auto";
-                promptRef.current.style.height = `${promptRef.current.scrollHeight}px`;
-              }}
-              placeholder="Message ChatGPT..."
-            />
-            <div className={clsx("send-btn")} onClick={handlePromptData}>
-              <FaArrowUpLong />
+            <div className="edit-icon">
+              <FiEdit />
             </div>
           </div>
-          <p className="disclaimer">
-            ChatGPT Clone may produce inaccurate information about people,
-            places, or facts.
-          </p>
+          <div className="chat-history-container">
+            {chatHistory.length > 0
+              ? chatHistory.map((item) => (
+                  <div
+                    key={item.historyId}
+                    className="title-container rounded-xl mt-1 relative"
+                    onClick={() => handleOldChatClick(item.historyId)}
+                  >
+                    <p className="title">{item.title}</p>
+                    <div className="absolute top-3 right-0 bg-gradient-to-r from-slate-50/15 to-slate-900/75 settings">
+                      <Popover>
+                        <PopoverTrigger>
+                          <PiDotsThreeBold className="fill-white	" />
+                        </PopoverTrigger>
+                      </Popover>
+                    </div>
+                  </div>
+                ))
+              : null}
+          </div>
+        </div>
+
+        <div className="chat-container">
+          <div className="d-flex p-2 justify-between">
+            <Popover>
+              <PopoverTrigger className="focus:bg-zinc-100 rounded-lg">
+                <div className="d-flex items-center gap-2 p-2 ">
+                  <span className="font-bold">ChatGPT 3.5</span>
+                  <MdOutlineKeyboardArrowDown />
+                </div>
+              </PopoverTrigger>
+              <PopoverContent className="w-80 p-0" align="start">
+                <div className="p-2">
+                  <div className="d-flex justify-between items-center gap-2.5 p-3 hover:bg-zinc-100 rounded-lg cursor-pointer">
+                    <div className="d-flex flex-col	">
+                      <span className="font-bold">GPT-3.5</span>
+                      <span className="text-sm text-muted-foreground">
+                        Great for everyday tasks.
+                      </span>
+                    </div>
+                    <div>
+                      <Checkbox className="rounded-full" checked />
+                    </div>
+                  </div>
+                  <Dialog modal>
+                    <DialogTrigger asChild>
+                      <div
+                        className="d-flex justify-between mt-1.5 items-center gap-2.5 p-3 hover:bg-zinc-100 rounded-lg cursor-pointer"
+                        onClick={togglePricingModal}
+                      >
+                        <div className="d-flex flex-col	">
+                          <span className="font-bold">GPT-4</span>
+                          <span className="text-sm text-muted-foreground">
+                            Our smartes and most capable model. Includes DALL-E,
+                            browsing and more
+                          </span>
+                          <Button className="mt-1 bg-violet-500">
+                            Upgrade to Plus
+                          </Button>
+                        </div>
+                        <div>
+                          <Checkbox className="rounded-full" disabled />
+                        </div>
+                      </div>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Upgrade your plan</DialogTitle>
+                      </DialogHeader>
+                      <div className="d-flex border-y border-y-slate-900">
+                        <div className="d-flex flex-col">hello</div>
+                      </div>
+                      <DialogFooter>
+                        <span>
+                          Need more capabilities? See ChatGPT EnterPrise
+                        </span>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+                </div>
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="chat-response-container">
+            {history.length > 0 ? (
+              history.map((item, index) => (
+                <>
+                  <div
+                    key={index + 1}
+                    className={clsx(
+                      "response-container w-full max-w-[768px] mx-auto",
+                      item.role
+                    )}
+                  >
+                    <div
+                      className={clsx("chat-response", {
+                        "show-edit-icon": item.role === "user",
+                      })}
+                    >
+                      <div className="user-details">
+                        <div className="icon">
+                          {item.role === "user" ? (
+                            <IoPerson />
+                          ) : (
+                            <SiSendinblue />
+                          )}
+                        </div>
+                        <span>{item.role === "user" ? "You" : "ChatGPT"}</span>
+                      </div>
+                      <div className="response">
+                        {item.role === "user" ? (
+                          <p>{item.parts}</p>
+                        ) : (
+                          item.parts
+                            .split(/(```[^`]+```)/)
+                            .map((data) => renderGptResponse(data))
+                        )}
+                      </div>
+                      <div className="tools">
+                        {item.role === "user" ? (
+                          <FiEdit2 className="stroke-gray-500" />
+                        ) : (
+                          <CopyToClipboard
+                            text={item.parts}
+                            onCopy={(text, result) =>
+                              setGptResponseCopied(result)
+                            }
+                          >
+                            {gptResponseCopied ? (
+                              <IoMdCheckmark className="fill-gray-500" />
+                            ) : (
+                              <GoPaste className="fill-gray-500" />
+                            )}
+                          </CopyToClipboard>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  {index === history.length - 1 && (
+                    <>
+                      <div className="last-section" />
+                      <div ref={bottomRef}></div>
+                    </>
+                  )}
+                </>
+              ))
+            ) : (
+              <div className="default-screen">
+                <div className="icon">
+                  <SiSendinblue />
+                </div>
+                <h2 className="text-3xl mt-1">How can I help you today?</h2>
+              </div>
+            )}
+          </div>
+
+          <div className="input-container">
+            <div className="interact w-full max-w-[768px] mx-auto relative">
+              <textarea
+                type="text"
+                name="prompt"
+                rows={1}
+                ref={promptRef}
+                onInput={(e) => {
+                  promptRef.current.style.height = "auto";
+                  promptRef.current.style.height = `${promptRef.current.scrollHeight}px`;
+                }}
+                placeholder="Message ChatGPT..."
+              />
+              <div className={clsx("send-btn")} onClick={handlePromptData}>
+                <FaArrowUpLong />
+              </div>
+            </div>
+            <p className="disclaimer w-full max-w-[768px] mx-auto">
+              ChatGPT Clone may produce inaccurate information about people,
+              places, or facts.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
